dd7ea58c282a738ce838b10632443d34c98ff30a
Robert Kuzma <robert@balavec.com>
2019-11-20 10:08:19 +0100

    Remove py27 and fix tests

--- tests/test_randomjoke.py
+++ tests/test_randomjoke.py
@@ -28,7 +28,6 @@ class TestRandomjoke(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'randomjoke.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
